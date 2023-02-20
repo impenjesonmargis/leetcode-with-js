609b952ab5aefd446bcbe20bfa792d68e49a4103
আলি হাসান <101947194+dev-alihasan@users.noreply.github.com>
2023-02-21 00:34:11 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const strs1 = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(strs1)); // Output: "fl"
+
+const strs2 = ["dog", "racecar", "car"];
+console.log(longestCommonPrefix(strs2)); // Output: ""
